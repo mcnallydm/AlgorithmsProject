6d4b429dee8c7924755a16586a778d980572ed62
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-18 13:22:22 +0200

    Added sorted names

---
+++ Q1A_Unsorted.c
@@ -0,0 +1,11 @@
+/*
+- Minimize average distance to all houses
+- Distance calculated using Manhattan method D = |x1-x2| + |y1-y2|
+- Input as array of x-values and array of y-values
+
+- Average of x-values = police-x
+- Average of y-values = police-y
+- Sorting doesn't matter
+*/
+#include <math.h>
+
