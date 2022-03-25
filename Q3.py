fdf343e3a28f5c87a4716057f04bc3b35d11f87c
DM <mcnally@udel.edu>
2022-03-25 23:11:08 +0200

    debugging start/end issues

--- Q3.py
+++ Q3.py
@@ -33,28 +33,25 @@ Idea 3:
             elif leaf2==-1 or idx[3]+idx[4]>leaf2_cost:
                 leaf2 = idx
                 leaf2_cost = idx[3]+idx[4]
-    return [leaf1, leaf2]
+    return [leaf1, leaf2]'''
 
-def find_ends(alist):
+def find_start(alist, end1, end2):
     # Sums the effort to clear and traverse a path and returns the index of the node with the highest minimum
-    highest_min_out = 0
-    high_min_out2 = 0
-    min_in_out = -1
-    leaf1 = -1  # Index of Leaf with the highest cost
-    leaf1_cost = -1
-    leaf2 = -1  # Index of Leaf with the second highest cost
-    leaf2_cost = -1
-    start_idx = -1
-    end_idx = -1
-    for idx in range(0, len(alist)):
-        for dist_idx in range(2, len(alist[idx])-1, 3):
-            if (alist[idx][dist_idx]+alist[idx][dist_idx+1])<min_in_out or min_in_out<0:
-                min_in_out = alist[idx][dist_idx]+alist[idx][dist_idx+1]    # Calculates the minimum clear/traverse total to/from that node
-        if min_in_out>highest_min_out:
-            highest_min_out = min_in_out    # Finds the max min clear/traverse of the whole graph
-            start_idx = idx
-        min_in_out = -1
-    return [start_idx, end_idx]'''
+    min_in_out1 = -1
+    min_in_out2 = -1
+    start_idx = end1
+    end_idx = end2
+    for dist_idx in range(2, len(alist[end1])-1, 3):
+        if (alist[end1][dist_idx]+alist[end1][dist_idx+1])<min_in_out1 or min_in_out1<0:
+            min_in_out1 = alist[end1][dist_idx]+alist[end1][dist_idx+1]    # Calculates the minimum clear/traverse total to/from that node
+    for dist_idx in range(2, len(alist[end2])-1, 3):
+        if (alist[end2][dist_idx]+alist[end2][dist_idx+1])<min_in_out2 or min_in_out2<0:
+            min_in_out2 = alist[end2][dist_idx]+alist[end2][dist_idx+1]    # Calculates the minimum clear/traverse total to/from that node
+    if min_in_out2<min_in_out1:
+        temp = start_idx
+        start_idx = end_idx
+        end_idx = temp
+    return [start_idx, end_idx]
 
 def find_ends(alist):
     start_idx = 0
@@ -67,6 +64,7 @@ def find_ends(alist):
             start_idx = min_path[0]
             end_idx = min_path[1]
             total = min_path[2]
+    print(start_idx, end_idx)
     return [start_idx, end_idx]
 
 def mst_traverse(file):
@@ -74,9 +72,9 @@ def mst_traverse(file):
     alist = fromFile(file)
     num_nodes = alist[0]
     alist = alist[1]
+    #first_node = find_start(alist)
     first_node = find_ends(alist)[0]
-    last_node = find_ends(alist)[1]
-    #print("Starting node:", find_prev(first_node))
+    print("Starting node:", find_prev(first_node))
     visited = [first_node]    # List of indexes of visited nodes
     check_visited = [0] * num_nodes   # Binary hash map of nodes using the ascii values
     check_visited[first_node] = 1
@@ -145,6 +143,7 @@ def find_min_path(alist, start):
     num_nodes = len(alist)
     visited = [start]    # List of indexes of visited nodes
     to_visited = [0] * num_nodes
+    inout = [0] * num_nodes
     to_visited[start] = 0
     check_visited = [0] * num_nodes   # Binary hash map of nodes using the ascii values
     check_visited[start] = 1
@@ -155,16 +154,18 @@ def find_min_path(alist, start):
             node = visited[nodeidx]
             #print(alist[node])
             for connected in range(1, len(alist[node]), 3):   # Gets connected node letter
-                if check_visited[asciindex(alist[node][connected])]!=1 or (to_visited[asciindex(alist[node][connected])]==0 or to_visited[asciindex(alist[node][connected])]>to_visited[node]+alist[node][connected+1]):
-                    to_visited[asciindex(alist[node][connected])] = to_visited[node]+alist[node][connected+1]
+                if check_visited[asciindex(alist[node][connected])]!=1 and (to_visited[asciindex(alist[node][connected])]==0 or to_visited[asciindex(alist[node][connected])]>to_visited[node]+alist[node][connected+1] or inout[asciindex(alist[node][connected])]>inout[node]+alist[node][connected+1]+alist[node][connected+2]):
+                    to_visited[asciindex(alist[node][connected])] = to_visited[node] + alist[node][connected+1]
+                    inout[asciindex(alist[node][connected])] = inout[node] + alist[node][connected+1] + alist[node][connected+2]
                     #print("Distance from",find_prev(start), "to", find_prev(node), "to", alist[node][connected], "is:", to_visited[node], "+", alist[node][connected+1], "=", to_visited[asciindex(alist[node][connected])])
                     visited.append(asciindex(alist[node][connected]))
                     check_visited[asciindex(alist[node][connected])] = 1
                     '''if furthest==-1 or to_visited[asciindex(alist[node][connected])]>total:
                         furthest = asciindex(alist[node][connected])
                         total = to_visited[asciindex(alist[node][connected])]'''
-    total = max(to_visited)
-    furthest = to_visited.index(total)
+    total = max(inout)
+    furthest = inout.index(total)
+    
     #print(find_prev(start), find_prev(furthest), total, to_visited)
     return [start, furthest, total, to_visited]
     #return [start, furthest, total]
--- Test2.py
+++ Test2.py
@@ -9,7 +9,22 @@ for letter in abc:
 for idx in ascidx:
     print(find_prev(idx))'''
 
-print(mst_traverse("Example.txt"))
+#print(mst_traverse("Example.txt"))
 '''ex = fromFile("Q3Test1.txt")[1]
 for node in range(0, len(ex)):
     print(find_min_path(ex, node))'''
+
+'''ex3 = fromFile("Q3Test3.txt")[1]
+for n in range(0, len(ex3)):
+    print(mst_traverse("Q3Test3.txt", n))
+    print("------------------------------------------------------------------")'''
+
+'''ex1 = fromFile("Q3Test1.txt")[1]
+for n in range(0, len(ex1)):
+    print(mst_traverse("Q3Test1.txt", n))
+    print("------------------------------------------------------------------")'''
+
+'''exam = fromFile("Example.txt")[1]
+for n in range(0, len(exam)):
+    print(mst_traverse("Example.txt", n))
+    print("------------------------------------------------------------------")'''
