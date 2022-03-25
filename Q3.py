8b311b3d4c273686a66ab4c76f5094619b280d42
DM <mcnally@udel.edu>
2022-03-25 21:15:10 +0200

    added broken but no runtime error q3

--- Q3.py
+++ Q3.py
@@ -2,7 +2,23 @@
 from Q2 import fromFile, asciindex, find_prev
 import sys
 
-def find_leaves(alist):
+'''
+Idea 1:
+    Check list of unvisited nodes
+    Calculate the distance to each node from everywhere along current path
+    Take smallest
+
+Idea 2:
+    Brute force it
+    Take all the paths of length n-1 that go through all nodes
+    Find the shortest
+
+Idea 3:
+    Find the 2 nodes that have the longest shortest distance between them?
+    They are the start & end?
+'''
+
+'''def find_leaves(alist):
     leaf1 = -1  # Index of Leaf with the highest cost
     leaf1_cost = -1
     leaf2 = -1  # Index of Leaf with the second highest cost
@@ -38,6 +54,19 @@ def find_ends(alist):
             highest_min_out = min_in_out    # Finds the max min clear/traverse of the whole graph
             start_idx = idx
         min_in_out = -1
+    return [start_idx, end_idx]'''
+
+def find_ends(alist):
+    start_idx = 0
+    end_idx = 0
+    total = 0
+    min_path = []
+    for node_idx in range(0, len(alist)):
+        min_path = find_min_path(alist, node_idx)
+        if min_path[2]>total:
+            start_idx = min_path[0]
+            end_idx = min_path[1]
+            total = min_path[2]
     return [start_idx, end_idx]
 
 def mst_traverse(file):
@@ -55,22 +84,102 @@ def mst_traverse(file):
     next_node = ""
     previous_node = find_prev(first_node)
     total_dist = 0
+    traverse_dist = 0
+    opened = [[]] * num_nodes
+    t = 0
+    current_node = previous_node
+    for node in range(0, len(alist)):
+        opened[node] = [find_prev(node)]
     # Find first node's shortest path
+    while len(visited)!=num_nodes:
+        min_dist = -1
+        t = 0
+        traverse_dist = 0
+        next_node = ""
+        for idx in visited:
+            if idx!=current_node:
+                print(idx, find_prev(idx))
+                traverse_dist += find_min_path(opened, asciindex(current_node))[3][idx]
+            for dist_idx in range(2, len(alist[idx])-1, 3):    # Goes by every 3 elements, because that's the clear cost
+                traverse_dist += alist[idx][dist_idx]
+                print(traverse_dist)
+                if (traverse_dist<min_dist or min_dist<0) and check_visited[asciindex(alist[idx][dist_idx-1])]!=1:   # Makes sure it hasn't been visited before
+                    min_dist = alist[idx][dist_idx]
+                    t = alist[idx][dist_idx+1]
+                    next_node = alist[idx][dist_idx-1]
+                    previous_node = find_prev(idx)
+        current_node = next_node
+        opened[asciindex(previous_node)].append(next_node)
+        opened[asciindex(previous_node)].append(t)
+        opened[asciindex(previous_node)].append(t)
+        opened[asciindex(next_node)].append(previous_node)
+        opened[asciindex(next_node)].append(t)
+        opened[asciindex(next_node)].append(t)
+        check_visited[asciindex(next_node)] = 1
+        visited.append(asciindex(next_node))
+        output += previous_node + next_node + "\n"
+        total_dist += min_dist
+        print(opened)
+    print("Total Distance:", total_dist)
+    return output
+
+def find_min_path(alist, start):
+    #print(alist)
+    # Cycle through all children to find the endpoint
+    #furthest = -1
+    #total = -1
+    num_nodes = len(alist)
+    visited = [start]    # List of indexes of visited nodes
+    to_visited = [0] * num_nodes
+    to_visited[start] = 0
+    check_visited = [0] * num_nodes   # Binary hash map of nodes using the ascii values
+    check_visited[start] = 1
+    for idx in range(0, num_nodes):
+        # Dijkstra's
+        # Cycle through all branches to find that point
+        for nodeidx in range(idx, len(visited)):
+            node = visited[nodeidx]
+            #print(alist[node])
+            for connected in range(1, len(alist[node]), 3):   # Gets connected node letter
+                if check_visited[asciindex(alist[node][connected])]!=1 or (to_visited[asciindex(alist[node][connected])]==0 or to_visited[asciindex(alist[node][connected])]>to_visited[node]+alist[node][connected+1]):
+                    to_visited[asciindex(alist[node][connected])] = to_visited[node]+alist[node][connected+1]
+                    #print("Distance from",find_prev(start), "to", find_prev(node), "to", alist[node][connected], "is:", to_visited[node], "+", alist[node][connected+1], "=", to_visited[asciindex(alist[node][connected])])
+                    visited.append(asciindex(alist[node][connected]))
+                    check_visited[asciindex(alist[node][connected])] = 1
+                    '''if furthest==-1 or to_visited[asciindex(alist[node][connected])]>total:
+                        furthest = asciindex(alist[node][connected])
+                        total = to_visited[asciindex(alist[node][connected])]'''
+    total = max(to_visited)
+    furthest = to_visited.index(total)
+    print(find_prev(start), find_prev(furthest), total, to_visited)
+    return [start, furthest, total, to_visited]
+    #return [start, furthest, total]
+
+'''def find_path(alist, start_idx):
+    all_paths = []  # Format [dist, []]
+    path = []
+    total_dist = 0
+    num_nodes = len(alist)
+    visited = [0]    # List of indexes of visited nodes
+    check_visited = [0] * num_nodes   # Binary hash map of nodes using the ascii values
+    check_visited[0] = 1
+    max_cleared = num_nodes-1
+    opened = []  # List of cleared roads, elements: [start, end, traversal_cost]
+    current_node = start_idx
     while len(visited)!=num_nodes:
         min_dist = -1
         next_node = ""
         for idx in visited:
+            #print("idx: ", idx)
             for dist_idx in range(2, len(alist[idx])-1, 3):    # Goes by every 3 elements, because that's the clear cost
                 if (alist[idx][dist_idx]<min_dist or min_dist<0) and check_visited[asciindex(alist[idx][dist_idx-1])]!=1:   # Makes sure it hasn't been visited before
                     min_dist = alist[idx][dist_idx]
                     next_node = alist[idx][dist_idx-1]
                     previous_node = find_prev(idx)
+                    #print(asciindex(alist[idx][dist_idx-1]))
+        total_dist += min_dist
         check_visited[asciindex(next_node)] = 1
         visited.append(asciindex(next_node))
         output += previous_node + next_node + "\n"
         total_dist += min_dist
-    print("Total Distance:", total_dist)
-    return output
-
-'''datafile = sys.argv[1]
-print(mst_traverse(datafile))'''
+    return path'''
--- Test2.py
+++ Test2.py
@@ -1,10 +1,15 @@
-from Q2 import asciindex, find_prev, min_span_tree
-from Q3 import mst_traverse
-abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+from Q2 import *
+from Q3 import *
+'''abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ascidx = []
 for letter in abc:
     ascidx.append(asciindex(letter))
     print(asciindex(letter))
 
 for idx in ascidx:
-    print(find_prev(idx))
+    print(find_prev(idx))'''
+
+print(mst_traverse("Example.txt"))
+'''ex = fromFile("Q3Test1.txt")[1]
+for node in range(0, len(ex)):
+    print(find_min_path(ex, node))'''
